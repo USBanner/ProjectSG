e535886f17f1286331be02e634515bbfdbcb320a
USBanner <118328459+USBanner@users.noreply.github.com>
2023-01-14 09:41:49 -0500

    Update AboutCommand.ts

--- src/commands/general/AboutCommand.ts
+++ src/commands/general/AboutCommand.ts
@@ -23,23 +23,18 @@ const pkg: { version: string } = JSON.parse(
     usage: "{prefix}about"
 })
 export class AboutCommand extends BaseCommand {
-    public async execute(ctx: CommandContext): Promise<void> {
+    public execute(ctx: CommandContext): void {
         const values = [
             [i18n.__("commands.general.about.osUptimeString"), formatMS(uptime() * 1000)],
             [i18n.__("commands.general.about.processUptimeString"), formatMS(process.uptime() * 1000)],
             [i18n.__("commands.general.about.botUptimeString"), formatMS(process.uptime() * 1000)],
             [""],
-            [i18n.__("commands.general.about.cachedUsersString"), `${await this.client.utils.getUserCount()}`],
-            [i18n.__("commands.general.about.channelsString"), `${await this.client.utils.getChannelCount()}`],
-            [i18n.__("commands.general.about.serversString"), `${await this.client.utils.getGuildCount()}`],
-            [""],
             [i18n.__("commands.general.about.nodeVersionString"), process.versions.node],
             [i18n.__("commands.general.about.discordJSVersionString"), DJSVersion],
             [i18n.__("commands.general.about.ffmpegVersionString"), this.client.utils.getFFmpegVersion()],
             [i18n.__("commands.general.about.botVersionString"), pkg.version],
-            [i18n.__("commands.general.about.commitString"), this.client.utils.getCommitHash("HEAD")],
             [""],
-            [i18n.__("commands.general.about.sourceCodeString"), "https://github.com/Clytage/rawon"]
+            [i18n.__("commands.general.about.sourceCodeString"), "Powered by:  𝐔𝐒 𝐁𝐚𝐧𝐧𝐞𝐫"]
         ];
         const value = createTable(values);
 
